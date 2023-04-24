ea31aec0c8d457cfb1afda91c9686562d079cb02
raffaele-catalano <raf.catalano@gmail.com>
2023-04-24 22:26:07 +0200

    implemented markup + set axios + API call (to be setted)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,25 @@ createApp ({
 
     data() {
         return {
-            
+            mailList: [],
         }
+    },
+
+    methods: {
+        getMailList() {
+
+            const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+            for (let index = 0; index < 10; index++) {
+                axios.get(url).then((response) => {
+                    this.mailList.push(response.data.response);
+                })
+            }
+        }
+    },
+
+    mounted() {
+        this.getMailList()
     }
 
 
