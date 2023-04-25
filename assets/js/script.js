f6a2f27345c60ee9827b30da9fc709d495c9e9e3
raffaele-catalano <raf.catalano@gmail.com>
2023-04-25 11:51:13 +0200

    set-up API call function onClick + some changes in markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,36 @@ createApp ({
 
     methods: {
         getMailList() {
-
+            // per comodità viene inizializzata la variabile 'url' con il link
             const url = 'https://flynn.boolean.careers/exercises/api/random/mail';
-
+            // all'interno di un ciclo *for* viene richiamata l'API 
             for (let index = 0; index < 10; index++) {
-                axios.get(url).then((response) => {
-                    this.mailList.push(response.data.response);
+                axios.get(url).then((result) => {
+                    // il tutto viene pushato nell'array mailList,
+                    // che di partenza è vuoto
+                    this.mailList.push(result.data.response);
                 })
             }
+                // log inserito per verificare la corretta lunghezza dell'array 
+                console.warn('array dopo generate -->', this.mailList);
+        },
+
+        resetMailList() {
+            // questa funzioncina "svuota" l'array
+            this.mailList = [];
+
+                // log inserito per verificare se effettivamente l'array
+                // viene correttamente "svuotato" 
+                console.error('array dopo reset -->', this.mailList);
         }
     },
 
     mounted() {
-        this.getMailList()
+
+        // rimosso il richiamo alla funzione nel mounted affinchè questa
+        // possa "partire" al click sul *button*
+
+        // this.getMailList()
     }
 
 
